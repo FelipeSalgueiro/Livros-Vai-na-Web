2c73f163de56f872ec2db0028d7560138fa010ab
FelipeSalgueiro <felipemaiasalgueiro@gmail.com>
2024-11-21 14:19:17 -0300

    :lipstick: style: Estilização da pagina LivrosDoados

--- desafio3/src/Pages/LivrosDoados/LivrosDoados.jsx
+++ desafio3/src/Pages/LivrosDoados/LivrosDoados.jsx
@@ -1,15 +1,35 @@
 import livroProtagonista from "../../assets/livroProtagonista.png"
+import s from "./livrosDoados.module.scss"
 export default function LivrosDoados(){
     return(
-        <main>
+        <main className={s.mainLivrosDoados}>
             <h2>Livros Doados</h2>
-            <section className="listaLivros">
-                <section className="displayLivro">
+            <section className={s.listaLivros}>
+                <section className={s.displayLivro}>
                     <img src={livroProtagonista} alt="Imagem meramente ilustrativa do livro Protagonista."/>
                     <p>Protagonista</p>
                     <p>Susanne Andrade</p>
                     <p>Ficção</p>
                 </section>
+                <section className={s.displayLivro}>
+                    <img src={livroProtagonista} alt="Imagem meramente ilustrativa do livro Protagonista."/>
+                    <p>Protagonista</p>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </section>
+                <section className={s.displayLivro}>
+                    <img src={livroProtagonista} alt="Imagem meramente ilustrativa do livro Protagonista."/>
+                    <p>Protagonista</p>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </section>
+                <section className={s.displayLivro}>
+                    <img src={livroProtagonista} alt="Imagem meramente ilustrativa do livro Protagonista."/>
+                    <p>Protagonista</p>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </section>
+                {/*Aqui entraria outro livro doado*/}
             </section>
         </main>
     )
